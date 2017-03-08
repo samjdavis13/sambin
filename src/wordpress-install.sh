a5ac4f36ddb02f2c7027c10a39129ffc64503c83
Sam Davis <sam@sjd.co>
2017-03-08 16:41:31 +1100

    Add wordpress install script

---
+++ src/wordpress-install.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+###########################
+# Download Wordpress Core #
+###########################
+
+wp core download --locale=en_AU;
+
+
+########################
+# Create wp-config.php #
+########################
+
+# Ask for DB Name
+echo "Please enter new db name: "
+read database_name
+wp core config --dbname=$database_name --dbuser=root --dbpass='admin' --dbprefix=wp_forge_;
+
+# Create the database
+mysql -uroot -padmin -e "create database $database_name"
+
+
+##########################
+# Install Wordpress Core #
+##########################
+
+echo "Attempting to install wordpress..."
+wp core install --prompt --skip-themes --skip-plugins
+
+
+#######################
+# Remove site tagline #
+#######################
+
+wp option set blogdescription ""
+
+
+###########
+# Plugins #
+###########
+
+# Delete unwanted plugins
+wp plugin delete akismet hello
+
+# Download and activate Forge Tweaks Plugin
+git clone https://sd-forge@bitbucket.org/forgecollective/forge-tweaks.git wp-content/plugins/forge-tweaks
+wp plugin activate forge-tweaks
+
+# Download and install my list of frequently used plugins
+wp plugin install --activate https://github.com/wp-sync-db/wp-sync-db/archive/master.zip aryo-activity-log swifty-page-manager backupwordpress reveal-ids-for-wp-admin-25
+
+
+##########
+# Themes #
+##########
+
+# Ask for theme name
+echo "Please enter a name for the theme: "
+read theme_name
+git clone https://sd-forge@bitbucket.org/forgecollective/sam-bedrock.git wp-content/themes/$theme_name
+
+# Activate it
+wp theme activate $theme_name
+
+# Remove unwanted themes
+wp theme delete twentyseventeen twentysixteen twentyfifteen
+
+
+############
+# Complete #
+############
+
+# Open MAMP
+open /Applications/MAMP\ PRO/MAMP\ PRO.app/;
+
+# Alert the user on a job well done.
+echo "Wordpress Installed and Themes Downlaoded. All that's left is to create a virtual host in MAMP"
