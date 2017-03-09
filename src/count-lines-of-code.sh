827a27906dafd56a47ea1a2223a89b36a4e9b717
Sam Davis <sam@sjd.co>
2017-03-09 16:00:57 +1100

    Include .css files in code count

--- src/count-lines-of-code.sh
+++ src/count-lines-of-code.sh
@@ -1,2 +1,2 @@
-echo "ignoring: bower_components, node_modules & vendor dirs and .css files";
-cloc . --exclude-dir=bower_components,node_modules,vendor --exclude-ext=css
+echo "ignoring: bower_components, node_modules & vendor dirs";
+cloc . --exclude-dir=bower_components,node_modules,vendor
