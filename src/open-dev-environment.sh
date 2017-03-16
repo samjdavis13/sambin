c8c65db086d104f2905e77946cd6eb8206007a7a
Sam Davis <sam@sjd.co>
2017-03-17 10:39:20 +1100

    Add open dev environment script

---
+++ src/open-dev-environment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Open Tower
+echo "Opening Tower..."
+open /Applications/Tower.app
+
+# Open Mamp Pro
+echo "Opening MAMP Pro..."
+open /Applications/MAMP\ PRO/MAMP\ PRO.app
+
+# Open Codekit 3
+echo "Opening Codekit 3..."
+open /Applications/CodeKit\ 3.app
+
+# Open Transmit
+echo "Opening Transmit..."
+open /Applications/Transmit.app
